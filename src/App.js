4f791d5e6a4aa45e65c936053922427d9321f574
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-06-20 19:52:40 +0200

    impl getCountriesData

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { FormControl, MenuItem, Select } from "@material-ui/core";
+import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState(["us", "we", "ar"]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((res) => res.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <h1>Covid-19 tracker</h1>
+
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" value="abc">
+            {/* get all countries */}
+
+            {countries.map((country) => (
+              <MenuItem value="country">
+                {" "}
+                {country.value} {country.name}{" "}
+              </MenuItem>
+            ))}
+            {/* 
+          
+<MenuItem value="worldwide">worldwide</MenuItem>
+            <MenuItem value="worldwide">3</MenuItem>
+            <MenuItem value="worldwide">4</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
+
+      {/* header*/}
+      {/* title input  */}
+
+      {/* infobox */}
+      {/* infobox */}
+      {/* infobox */}
+
+      {/*table  */}
+      {/* graph */}
+
+      {/* map */}
     </div>
   );
 }
