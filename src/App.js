d402df7a5d891e8d2d73f161364b0939aded98f9
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-06-23 00:11:29 +0200

    impl map

--- src/App.js
+++ src/App.js
@@ -19,6 +19,9 @@ function App() {
   const [country, setCountry] = useState("worldwide");
   const [countryInfo, setCountryInfo] = useState({});
   const [tableData, setTableData] = useState([]);
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+  const [mapZoom, setMapZoom] = useState(2);
+  const [mapCountries, setMapCountries] = useState([]);
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
@@ -39,6 +42,7 @@ function App() {
           }));
           const sortedData = sortData(data);
           setTableData(sortedData);
+          setMapCountries(data);
           setCountries(countries);
         });
     };
@@ -59,6 +63,8 @@ function App() {
       .then((data) => {
         setCountry(countryCode);
         setCountryInfo(data);
+        setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+        setMapZoom(3);
       });
   };
 
@@ -114,7 +120,7 @@ function App() {
 
         {/* map */}
 
-        <Map />
+        <Map countries={mapCountries} center={mapCenter} zoom={mapZoom} />
       </div>
 
       <Card className="app__right">
--- src/util.js
+++ src/util.js
@@ -1,5 +1,45 @@
+import React from "react";
+import { Circle, Popup } from "react-leaflet";
+import numeral from "numeral";
+const casesTypeColors = {
+  cases: {
+    hex: "#0000ff",
+    rgb: "rgb(204, 16,52, 0.5)",
+    hal_op: "rgba(204,16,52,0.5)",
+    multiplier: 400,
+  },
+  recovered: {
+    hex: "#7ddd71d",
+    rgb: "rgb(125, 215, 29)",
+    halp_op: "rgba(125, 215, 29. 0.5)",
+    multiplier: 800,
+  },
+  deaths: {
+    hex: "#fb4443",
+    rgb: "rgb(251, 68, 67)",
+    half_op: "rgba(251, 68, 67, 0.5",
+    multiplier: 1500,
+  },
+};
+
 export const sortData = (data) => {
   const sortedData = [...data];
 
   return sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1));
 };
+export const showDataOnMap = (data, casesType = "cases") =>
+  data.map((country) => (
+    <Circle
+      center={[country.countryInfo.lat, country.countryInfo.long]}
+      color={casesTypeColors[casesType].hex}
+      fillColor={casesTypeColors[casesType].hex}
+      fillOpacity={0.4}
+      radius={
+        Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
+      }
+    >
+      <Popup>
+        <h1>im a popup</h1>
+      </Popup>
+    </Circle>
+  ));
